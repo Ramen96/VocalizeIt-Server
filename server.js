dcbd4a381eced1322db7148fad53863fe8ac29dd
Ramen96 <eric.worsfold@gmail.com>
2024-06-09 09:06:11 -0400

    fixed security flaw

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.post('/register', (req, res) => {
     .then(user => {
         res.json(user[0]);
     })
-    .catch(err => res.status(400).json(err))
+    .catch(err => res.status(400).json('Error: unable to join'))
 })
 
 app.get('profile/:id', (req, res) => {
