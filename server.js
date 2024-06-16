dde1795aaec5045220da76e76869a49db2b843ef
Ramen96 <eric.worsfold@gmail.com>
2024-06-16 17:50:34 -0400

    fix

--- server.js
+++ server.js
@@ -2,23 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex');
+const { Client } = require('pg');
 
 const signin = require('./controllers/signin');
 const register = require('./controllers/register');
 const profile =  require('./controllers/profile');
 const api = require('./controllers/api');
 
-const db = knex({
-    client: 'pg',
-    connection: {
-        host: process.env.DB_URL,
-        port: '5432',
-        user: 'eric',
-        database: 'vocalizeit',
-        password: process.env.DB_PASSWORD
-    },
+const db = new Client({
+  connectionString: process.env.PG_CONNECTION_URL,
 });
+
+db.connect()
+  .then(() => console.log('Connected to PostgreSQL database'))
+  .catch(err => console.error('Connection error', err.stack));
 
 const app = express();
 
@@ -26,11 +23,13 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('this is working'));
-app.post('/signin', (req, res) => {signin.handleSignIn(req, res, db, bcrypt)})
+app.post('/signin', (req, res) => {signin.handleSignIn(req, res, db, bcrypt)});
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
-app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db)})
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db)});
 app.post('/api', (req, res) => {api.handleApi(req, res, db, bcrypt)});
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`app is running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`app is running on port ${PORT}`);
 });
